e5890370b814eee943f2511bb0628a5204045a4c
knob-coder <fabianbonk11235@gmail.com>
2025-11-27 16:36:40 -0500

    Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,17 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj={ "raw_document": { "text": text_to_analyze } }
     header= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)  
-    return response.text
+    formatted_response = json.loads(response.text)
+    anger = formatted_response['emotionPredictions'][0]['emotion']["anger"]
+    disgust = formatted_response['emotionPredictions'][0]['emotion']["disgust"]
+    fear = formatted_response['emotionPredictions'][0]['emotion']["fear"]
+    joy = formatted_response['emotionPredictions'][0]['emotion']["joy"]
+    sadness = formatted_response['emotionPredictions'][0]['emotion']["sadness"]
+    emotion_set={'anger': anger, 'digust': disgust, "fear": fear, "joy": joy, "sadness": sadness}
+    dominant_emotion= max(emotion_set.items(), key=lambda item: item[1])
+    return {'anger': anger, 'disgust': disgust, "fear": fear, "joy": joy, "sadness": sadness, "dominant_emotion": dominant_emotion[0]}
